f3ab2a515e2b1ff577cf3359e7035a3ec49defdc
PravinSabariBala <pravinpsb16@gmail.com>
2023-08-19 10:41:51 +0530

    did first question

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+
+
+
+app.get('/numbers', async (req, res) => {
+    const urls = req.query.url;
+    console.log(urls)
+    if (!urls || !Array.isArray(urls)) {
+        return res.status(400).json({ error: 'Invalid input' });
+    }
+
+    const mergedNumbers = [];
+
+    const requests = urls.map(async (url) => {
+        try {
+            const response = await axios.get(url, { timeout: 500 });
+            if (response.status === 200) {
+                mergedNumbers.push(...response.data.numbers);
+            }
+        } catch (error) {
+            // Ignore URLs that take too long to respond
+        }
+    });
+
+    await Promise.all(requests);
+
+    const uniqueNumbers = Array.from(new Set(mergedNumbers));
+    uniqueNumbers.sort((a, b) => a - b);
+
+    res.json({ numbers: uniqueNumbers });
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
